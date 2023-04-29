a678bd6d98f052f55f5cd0b7102fa6cd21fd6ef9
djumanov <diyorbekjumanov1120@gmail.com>
2023-04-29 15:46:39 +0500

    model admin options

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'complete', 'created', 'updated')
+    list_filter = ('complete', 'created', 'updated')
+    search_fields = ('title', 'description', 'user__username')
+    ordering = ('complete', 'updated', 'created')
+    date_hierarchy = 'created'
+    list_display_links = ('title', 'description', 'created', 'updated')
+    actions = ('make_complete', 'make_incomplete')
+
+    @admin.action(description='Mark selected tasks as complete')
+    def make_complete(self, request, queryset):
+        queryset.update(complete=True)
+
+    @admin.action(description='Mark selected tasks as incomplete')
+    def make_incomplete(self, request, queryset):
+        queryset.update(complete=False)
