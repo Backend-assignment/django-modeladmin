f6227b7dd7d6e8d0d4555710ab28968bbe46bc87
ogabek <quvvatullayev@gmail.com>
2023-04-29 14:50:24 +0500

    init project

---
+++ api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
